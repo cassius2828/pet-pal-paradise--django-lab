81a140392d1c593843f3df2fcc065a397f0eb387
Cassius <cassius.reynolds.dev@gmail.com>
2024-07-19 14:52:23 -0700

    got home route and css to connect

--- main_app/apps.py
+++ main_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PetAdoptionConfig(AppConfig):
+class MainAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pet_adoption'
+    name = 'main_app'
