c411ef249a046cc8196db372fa3e11f2a8ee7eb9
Cassius <cassius.reynolds.dev@gmail.com>
2024-07-22 07:53:20 -0700

    added home screen, hero img, and added padding to view pets title

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('pets', views.pets_index, name="pets_index")
+    path('pets/', views.pets_index, name="pets_index"),
+    path('pets/:petId', views.pets_show, name="pets_show")
 ]
